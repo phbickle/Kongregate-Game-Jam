df7b076bc4837caddff0502db1af2fda32a7805f
lainezor <lainezor@live.ca>
2016-05-29 22:09:43 -0400

    scripts
    Laine's scripts that spawn stuff and things

---
+++ Attrative Clicker/Assets/Scripts/onDeath.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class onDeath : MonoBehaviour {
+
+	public GameObject[] droppableItems;
+	public GameObject[] droppableItemsClone;
+
+	int health = 5;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown () {
+		health -= 1;
+		if (health == 0) {
+			gameObject.active = false;//makes the enemy "disappear", should look into how to actually remove it so its not taking up any resources.
+			//could make the looting mechanism here
+			//droppableItemsClone [0] = Instantiate (droppableItems[0], Transform.position, Quaternion.identity) as GameObject;
+				}
+
+			}
+}
---
+++ Attrative Clicker/Assets/Scripts/spawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawner : MonoBehaviour {
+
+	public Transform[] spawnLocations;
+	public GameObject[] whatToSpawnPrefab;
+	public GameObject[] whatToSpawnClone;
+
+	void Start(){
+		spawnSomethingAwesomePlease ();
+		}
+
+	void spawnSomethingAwesomePlease(){
+		whatToSpawnClone [0] = Instantiate (whatToSpawnPrefab[0],spawnLocations[0].transform.position, Quaternion.Euler(0,0,0)) as GameObject;//make a clone array so you dont spawn the actual prefab, always spawn clones
+		whatToSpawnClone [1] = Instantiate (whatToSpawnPrefab[0],spawnLocations[1].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+		whatToSpawnClone [2] = Instantiate (whatToSpawnPrefab[0],spawnLocations[2].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+	}
+
+
+
+}
+
+
