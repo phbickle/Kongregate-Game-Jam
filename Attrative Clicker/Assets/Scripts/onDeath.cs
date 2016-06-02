c12711179d821ab0686efed4e96cb1e55fad6ce8
lainezor <lainezor@live.ca>
2016-06-02 15:00:41 -0400

    Monsters Drop Coins
    Monsters now will drop one coin each when they die. Also fixed the
    memory leak issue :+1:

--- Attrative Clicker/Assets/Scripts/onDeath.cs
+++ Attrative Clicker/Assets/Scripts/onDeath.cs
@@ -2,9 +2,12 @@
 using System.Collections;
 
 public class onDeath : MonoBehaviour {
+	
+	//public GameObject[] droppableItems;
+	//public GameObject[] droppableItemsClone;
+	public Transform monster;
+	public Transform coin;
 
-	public GameObject[] droppableItems;
-	public GameObject[] droppableItemsClone;
 
 	int health = 5;
 	// Use this for initialization
@@ -20,9 +23,11 @@ public class onDeath : MonoBehaviour {
 	void OnMouseDown () {
 		health -= 1;
 		if (health == 0) {
-			gameObject.active = false;//makes the enemy "disappear", should look into how to actually remove it so its not taking up any resources.
+			//gameObject.active = false;//makes the enemy "disappear", should look into how to actually remove it so its not taking up any resources.
+			Destroy(gameObject);
 			//could make the looting mechanism here
-			//droppableItemsClone [0] = Instantiate (droppableItems[0], Transform.position, Quaternion.identity) as GameObject;
+			//droppableItemsClone[0] = Instantiate (droppableItems[0], monster.transform.position, Quaternion.identity) as GameObject;
+			Instantiate ( coin, monster.transform.position, Quaternion.identity);
 				}
 
 			}
